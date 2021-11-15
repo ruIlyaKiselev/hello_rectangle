7c95e5679d701fb488300312d24390542db4f19c
Ilya Kiselev <ruilyakiselev@gmail.com>
2021-11-15 11:41:51 +0700

    some step

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello rectangle"),
-        ),
-        body: HelloRectangleBody(),
-      ),
-    )
-  );
-}
-
-class HelloRectangleBody extends StatelessWidget {
-  const HelloRectangleBody({Key? key}) : super(key: key);
-
+class HelloRectangle extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400,
-        width: 300,
-        child: Center(
-          child: Text(
-            "Hello!"
-          ),
-        ),
-      ),
-    )
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.redAccent,
+    );
   }
+}
+
+void main() {
+
+  runApp(
+      MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.green,
+                title: Text("Aaaaaaaa"),
+              ),
+              body: HelloRectangle()
+          )
+      )
+  );
 }
 
